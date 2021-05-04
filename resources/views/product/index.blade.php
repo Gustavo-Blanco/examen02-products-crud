2785ce7defa67e48d5139a19b6b50dfaa99c7d1f
gblanco <gustavo.blanco@tecsup.edu.pe>
2021-05-04 16:56:39 -0500

    test

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container p-4">
     <a href="{{route('products.create')}}" class="btn btn-outline-primary mb-5 p-2">Create A New Product</a>
     <table class="table border border-1 text-center shadow p-1">
-        <thead class="bg-secondary text-white">
+        <thead class="thead-background text-white">
             <tr>
                 <th scope="col">Id</th>
                 <th scope="col">Name</th>
@@ -23,11 +23,11 @@
                     <td>{{$product->price}}</td>
                     <td>{{$product->category}}</td>
                     <td class="d-flex justify-content-center">
-                        <a href="{{route('products.edit',$product)}}" class="btn btn-outline-info mx-1"><i class="far fa-edit"></i></a>
+                        <a href="{{route('products.edit',$product)}}" class="btn mx-1 icon-material"><i class="far fa-edit"></i></a>
                         <form action="{{route('products.destroy', $product)}}" method="post" class="mx-1">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></button>
+                            <button class="btn icon-material"><i class="far fa-trash-alt"></i></button>
                         </form>
                     </td>
                 </tr>
